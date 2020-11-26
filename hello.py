34f6b0e4b2c51854cc06c0210e2cc21e1f3004e9
satter0827 <brts50nw8q@gmail.com>
2020-11-26 18:39:42 +0900

    gunicornを使用

--- hello.py
+++ hello.py
@@ -14,6 +14,4 @@ def good():
 
 ## おまじない
 if __name__ == "__main__":
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
