39c9813958a9366846650973e661ce5e8523c69c
satter0827 <brts50nw8q@gmail.com>
2020-12-02 11:20:01 +0900

    IBMクラウド向けのwebhookを追加

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort
-import os
+import os, requests, json
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -37,6 +37,15 @@ def callback():
     # handle webhook body
     try:
         handler.handle(body, signature)
+
+        WEB_HOOK_URL = "{Webhook URL}"
+            requests.post(WEB_HOOK_URL, data = json.dumps({
+            'text': u'Notifycation From Python.',  #通知内容
+            'username': u'Bakira-Tech-Python-Bot',  #ユーザー名
+            'icon_emoji': u':smile_cat:',  #アイコン
+            'link_names': 1,  #名前をリンク化
+        }))
+        
     except InvalidSignatureError:
         abort(400)
 
