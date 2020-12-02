e998168c062aee68d4b6b873eb0ff5cc020bafd1
satter0827 <brts50nw8q@gmail.com>
2020-12-02 10:08:31 +0900

    メッセージの文言を修正

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if (event.message.text == "座禅を初めて"):
+    if (event.message.text == "座禅を始めて"):
         hento = "座禅を始めます"
     else:
         hento = event.message.text
