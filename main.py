6a433a9ece07ef0069b5ecc339d8be43255fa471
satter0827 <brts50nw8q@gmail.com>
2020-12-02 11:33:27 +0900

    IBMクラウド向けwebhookを作成

--- main.py
+++ main.py
@@ -38,12 +38,10 @@ def callback():
     try:
         handler.handle(body, signature)
 
-        WEB_HOOK_URL = "{Webhook URL}"
-            requests.post(WEB_HOOK_URL, data = json.dumps({
-            'text': u'Notifycation From Python.',  #通知内容
-            'username': u'Bakira-Tech-Python-Bot',  #ユーザー名
-            'icon_emoji': u':smile_cat:',  #アイコン
-            'link_names': 1,  #名前をリンク化
+        WEB_HOOK_URL = "https://yuasa-test-app01.us-south.cf.appdomain.cloud/"
+        requests.post(WEB_HOOK_URL, data = json.dumps({
+            'text': u'Notifycation From Heroku.',  #通知内容
+            'username': u'osho-line-bot',  #ユーザー名
         }))
         
     except InvalidSignatureError:
